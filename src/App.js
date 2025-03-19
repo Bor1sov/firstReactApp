e211117ca6bd9b09428f42d68027887ce78473d9
Egor <nd.5nd@yandex.ru>
2025-03-19 22:12:49 +0300

    JSX free

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import { getYear } from './Date';
+import { createElement } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-		<div className='year-now'>{getYear()}</div>
-      </header>
-    </div>
-  );
+	const appDiv = createElement('div', { className: 'App' },
+		createElement('header', { className: 'App-header' },
+		  createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+		  createElement('p', null, // null means no attributes
+			'Edit src/App.js and save to reload.',
+			createElement('code', null, 'src/App.js')
+		  ),
+		  createElement('a', {
+			className: 'App-link',
+			href: 'https://reactjs.org',
+			target: '_blank',
+			rel: 'noopener noreferrer'
+		  }, 'Learn React'),
+		  createElement('div', { className: 'year-now' }, getYear())
+		)
+	  );
+
+	  return appDiv;
 }
 
 export default App;
